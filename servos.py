ce9a7f142295e80e39675e43bab32e2e05a2a0dc
Kyle Hemsley <hemsleyk@users.noreply.github.com>
2019-01-24 14:51:02 -0500

    Skeleton servos.py from example program

--- servos.py
+++ servos.py
@@ -1,1 +1,54 @@
+# This is the servo library for the course, adapted from the example file.
+# See https://learn.adafruit.com/adafruit-16-channel-pwm-servo-hat-for-raspberry-pi/ for more details.
 
+import time
+import Adafruit_PCA9685
+import signal
+import math
+
+# The servo hat uses its own numbering scheme within the Adafruit library.
+# 0 represents the first servo, 1 for the second, and so on.
+LSERVO = 0
+RSERVO = 1
+
+# This function is called when Ctrl+C is pressed.
+# It's intended for properly exiting the program.
+# Need to remove this in production most likely.
+def ctrlC(signum, frame):
+    print("Exiting")
+    
+    # Stop the servos
+    pwm.set_pwm(LSERVO, 0, 0);
+    pwm.set_pwm(RSERVO, 0, 0);
+    
+    exit()
+
+# Attach the Ctrl+C signal interrupt
+# Need to remove this in production most likely.
+signal.signal(signal.SIGINT, ctrlC)
+    
+# Initialize the servo hat library.
+# 50Hz is used for the frequency of the servos.
+pwm = Adafruit_PCA9685.PCA9685()
+pwm.set_pwm_freq(50)
+
+# Write an initial value of 1.5, which keeps the servos stopped.
+# Due to how servos work, and the design of the Adafruit library, 
+# the value must be divided by 20 and multiplied by 4096.
+pwm.set_pwm(LSERVO, 0, math.floor(1.5 / 20 * 4096));
+pwm.set_pwm(RSERVO, 0, math.floor(1.5 / 20 * 4096));
+
+while True:
+    # Write a maximum value of 1.7 for each servo.
+    # Since the servos are oriented in opposite directions,
+    # the robot will end up spinning in one direction.
+    # Values between 1.3 and 1.7 should be used.
+    pwm.set_pwm(LSERVO, 0, math.floor(1.6 / 20 * 4096));
+    pwm.set_pwm(RSERVO, 0, math.floor(1.6 / 20 * 4096));
+    time.sleep(4)
+    
+    # Write a minimum value of 1.4 for each servo.
+    # The robot will end up spinning in the other direction.
+    pwm.set_pwm(LSERVO, 0, math.floor(1.4 / 20 * 4096));
+    pwm.set_pwm(RSERVO, 0, math.floor(1.4 / 20 * 4096));
+    time.sleep(4)
