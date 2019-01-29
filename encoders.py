90ecb6d0181bd96025c23b06772613f04fef35a8
Devin369 <47009418+Devin369@users.noreply.github.com>
2019-01-29 15:47:35 -0500

    Update encoders.py

--- encoders.py
+++ encoders.py
@@ -1,1 +1,47 @@
+# This program demonstrates usage of the digital encoders.
+# After executing the program, manually spin the wheels and observe the output.
+# See https://sourceforge.net/p/raspberry-gpio-python/wiki/Inputs/ for more details.
+
+import time
+import RPi.GPIO as GPIO
+import signal
+
+# Pins that the encoders are connected to
+LENCODER = 17
+RENCODER = 18
+
+# This function is called when the left encoder detects a rising edge signal.
+def onLeftEncode(pin):
+    print("Left encoder ticked!")
+
+# This function is called when the right encoder detects a rising edge signal.
+def onRightEncode(pin):
+    print("Right encoder ticked!")
+
+# This function is called when Ctrl+C is pressed.
+# It's intended for properly exiting the program.
+def ctrlC(signum, frame):
+    print("Exiting")
+    GPIO.cleanup()
+    exit()
+
+# Attach the Ctrl+C signal interrupt
+signal.signal(signal.SIGINT, ctrlC)
+    
+# Set the pin numbering scheme to the numbering shown on the robot itself.
+GPIO.setmode(GPIO.BCM)
+
+# Set encoder pins as input
+# Also enable pull-up resistors on the encoder pins
+# This ensures a clean 0V and 3.3V is always outputted from the encoders.
+GPIO.setup(LENCODER, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(RENCODER, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+# Attach a rising edge interrupt to the encoder pins
+GPIO.add_event_detect(LENCODER, GPIO.RISING, onLeftEncode)
+GPIO.add_event_detect(RENCODER, GPIO.RISING, onRightEncode)
+
+# Prevent the program from exiting by adding a looping delay.
+while True:
+    time.sleep(1)
 
