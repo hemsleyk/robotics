e60cd0c33ace380044e39c52d06530b06b004054
Kyle Hemsley <kyle@hemsley.tech>
2019-02-05 16:05:26 -0500

    namespaces making sense

--- Forward.py
+++ Forward.py
@@ -1,11 +1,11 @@
-from lib import encoders
+from lib import encoders, servos
 
 def MoveForward(inches, seconds):
     counts = encoders.getCounts()
     #stop at ticks*IN_PER_TICK>=inches
-    SetSpeeds(100,100)
-    while(counts[0] >= inches*IN_PER_TICK or counts[1] >= inches*IN_PER_TICK): counts = lib.encoders.getCounts()
-    SetSpeeds(0,0)
+    servos.SetSpeeds(100,100)
+    while(counts[0] >= inches*IN_PER_TICK or counts[1] >= inches*IN_PER_TICK): counts = encoders.getCounts()
+    servos.SetSpeeds(0,0)
 
 MoveForward(12, 999999)
 
