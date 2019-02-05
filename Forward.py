b845674e2c50ad6d8844e6b6049b573af80053a6
Kyle Hemsley <kyle@hemsley.tech>
2019-02-05 15:55:27 -0500

    test

--- Forward.py
+++ Forward.py
@@ -1,10 +1,10 @@
 import lib
 
 def MoveForward(inches, seconds):
-    counts = getCounts()
+    counts = lib.getCounts()
     #stop at ticks*IN_PER_TICK>=inches
     SetSpeeds(100,100)
-    while(counts[0] >= inches*IN_PER_TICK or counts[1] >= inches*IN_PER_TICK): counts = getCounts()
+    while(counts[0] >= inches*IN_PER_TICK or counts[1] >= inches*IN_PER_TICK): counts = lib.getCounts()
     SetSpeeds(0,0)
 
 MoveForward(12, 999999)
