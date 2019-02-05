752574ae78e25bed6c26abf63e38bb121ebf2873
Kyle Hemsley <kyle@hemsley.tech>
2019-02-05 15:44:29 -0500

    Re-organize project files

--- Lab1/Forward.py
+++ Lab1/Forward.py
@@ -1,1 +1,13 @@
+import ../lib/encoders.py
+import ../lib/servos.py
 
+def MoveForward(inches, seconds):
+    counts
+    #stop at ticks*IN_PER_TICK>=inches
+    SetSpeeds(100,100)
+    while(counts[0] >= inches*IN_PER_TICK or counts[1] >= inches*IN_PER_TICK): counts = getCounts()
+    SetSpeeds(0,0)
+
+MoveForward(12, 999999)
+
+            
