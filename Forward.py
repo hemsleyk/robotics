fcbfa1bdf4e3f0574bd407c976505678a55c93c0
Kyle Hemsley <kyle@hemsley.tech>
2019-02-05 16:06:18 -0500

    Fix setspeeds caps

--- Forward.py
+++ Forward.py
@@ -3,9 +3,9 @@ from lib import encoders, servos
 def MoveForward(inches, seconds):
     counts = encoders.getCounts()
     #stop at ticks*IN_PER_TICK>=inches
-    servos.SetSpeeds(100,100)
+    servos.setSpeeds(100,100)
     while(counts[0] >= inches*IN_PER_TICK or counts[1] >= inches*IN_PER_TICK): counts = encoders.getCounts()
-    servos.SetSpeeds(0,0)
+    servos.setSpeeds(0,0)
 
 MoveForward(12, 999999)
 
