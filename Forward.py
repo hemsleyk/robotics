8ab9ba3fe2698cb7fbd72a2b2ca30891759d464d
Kyle Hemsley <kyle@hemsley.tech>
2019-02-05 16:13:46 -0500

    try and condition

--- Forward.py
+++ Forward.py
@@ -4,7 +4,7 @@ def MoveForward(inches, seconds):
     counts = encoders.getCounts()
     #stop at ticks*IN_PER_TICK>=inches
     servos.setSpeeds(100,100)
-    while(counts[0] <= inches/servos.IN_PER_TICK or counts[1] <= inches/servos.IN_PER_TICK): counts = encoders.getCounts()
+    while(counts[0] <= inches/servos.IN_PER_TICK and counts[1] <= inches/servos.IN_PER_TICK): counts = encoders.getCounts()
     servos.setSpeeds(0,0)
 
 MoveForward(12, 999999)
