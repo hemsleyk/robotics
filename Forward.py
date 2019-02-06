cc5f1841657530df6d81cd31d250b0c335e4d2eb
Kyle Hemsley <kyle@hemsley.tech>
2019-02-05 20:37:51 -0500

    More debug

--- Forward.py
+++ Forward.py
@@ -10,6 +10,7 @@ def MoveForward(inches=0.0, seconds=0.0):
         print("Requested linear velocity:",ips)
         return 1
     else: #stop at ticks*IN_PER_TICK>=inches
+        print("Beginning forwards motion of",inches,"over",seconds,"seconds with a velocity of",ips,"in/s")
         servos.setSpeedsIPS(ips,ips) #set velocity
         while(counts[0] <= inches/servos.IN_PER_TICK or counts[1] <= inches/servos.IN_PER_TICK): counts = encoders.getCounts()
         servos.setSpeeds(0,0)
