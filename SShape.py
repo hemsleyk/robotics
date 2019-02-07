e7d2457f43cce372897c84b52ae5b03c6f3a4ab5
Kyle Hemsley <kyle@hemsley.tech>
2019-02-06 20:00:37 -0500

    Portion up time for W

--- SShape.py
+++ SShape.py
@@ -4,8 +4,13 @@ import math
 def MoveSShape(R1, R2, Y):
     encoders.resetCounts()
     counts = encoders.getCounts()
-    D = math.pi*(R1+R2)
+    D1 = math.pi*R1
+    D2 = math.pi*R2
+    D = D1+D2
     v = D/Y
+    Y1 = Y*R1/D
+    Y2 = Y*R2/D
+
     if(v > servos.LINEAR_V_MAX):
         print("Requested action exceeds theoretical maximum velocity")
         print("Maximum velocity:",servos.LINEAR_V_MAX)
@@ -13,10 +18,10 @@ def MoveSShape(R1, R2, Y):
         return 1
     else: #stop at ticks*IN_PER_TICK>=inches
         print("Beginning s-shaped motion of",D,"inches over",Y,"seconds with a velocity of",v,"in/s")
-        servos.setSpeedsVW(v,((math.pi*R1)/Y)/R1) #set velocity
+        servos.setSpeedsVW(v,(D1/Y1)/R1) #set velocity
         while(counts[0] <= math.pi*R1/servos.IN_PER_TICK or counts[1] <= math.pi*R1/servos.IN_PER_TICK): counts = encoders.getCounts()
         encoders.resetCounts()
-        servos.setSpeedsVW(v,-((math.pi*R2)/Y)/R2)
+        servos.setSpeedsVW(v,-(D2/Y2)/R2)
         while(counts[0] <= math.pi*R2/servos.IN_PER_TICK or counts[1] <= math.pi*R2/servos.IN_PER_TICK): counts = encoders.getCounts()
         servos.setSpeeds(0,0)
         return 0
