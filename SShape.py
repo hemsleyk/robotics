f3351eac6595c3a02ef5c05981849a6fb17a307a
Kyle Hemsley <kyle@hemsley.tech>
2019-02-06 20:09:17 -0500

    reset and stop.

--- SShape.py
+++ SShape.py
@@ -1,5 +1,6 @@
 from lib import encoders, servos
 import math
+import time
 
 def MoveSShape(R1, R2, Y):
     encoders.resetCounts()
@@ -20,6 +21,8 @@ def MoveSShape(R1, R2, Y):
         print("Beginning s-shaped motion of",D,"inches over",Y,"seconds with a velocity of",v,"in/s")
         servos.setSpeedsVW(v,(v/R1)) #set velocity
         while(counts[0] <= math.pi*R1/servos.IN_PER_TICK or counts[1] <= math.pi*R1/servos.IN_PER_TICK): counts = encoders.getCounts()
+        servos.setSpeeds(0,0)
+        time.sleep(0.1)
         encoders.resetCounts()
         input("Press enter to continue")
         servos.setSpeedsVW(v,(v/R2))
