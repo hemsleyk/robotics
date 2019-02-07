d1f9b4a0fdc6530a657051f7f32c105f345d4464
Kyle Hemsley <kyle@hemsley.tech>
2019-02-06 19:33:43 -0500

    S Shape

--- SShape.py
+++ SShape.py
@@ -13,10 +13,10 @@ def MoveSShape(R1, R2, Y):
         return 1
     else: #stop at ticks*IN_PER_TICK>=inches
         print("Beginning s-shaped motion of",D,"inches over",Y,"seconds with a velocity of",v,"in/s")
-        servos.setSpeedsVW(v,v/R1) #set velocity
+        servos.setSpeedsVW(v,(math.pi*R1)/R1) #set velocity
         while(counts[0] <= math.pi*R1/servos.IN_PER_TICK or counts[1] <= math.pi*R1/servos.IN_PER_TICK): counts = encoders.getCounts()
         encoders.resetCounts()
-        servos.setSpeedsVW(v,-v/R2)
+        servos.setSpeedsVW(v,-(math.pi*R2)/R2)
         while(counts[0] <= math.pi*R2/servos.IN_PER_TICK or counts[1] <= math.pi*R2/servos.IN_PER_TICK): counts = encoders.getCounts()
         servos.setSpeeds(0,0)
         return 0
