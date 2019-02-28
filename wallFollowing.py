655796cdf87fddf6ed6e76d75f8066111c0bd7a6
Kyle Hemsley <kyle@hemsley.tech>
2019-02-28 04:42:51 -0500

    smoothing

--- wallFollowing.py
+++ wallFollowing.py
@@ -14,10 +14,10 @@ else:
         YtdL = distance.lSensor.get_distance()/25.4 #convert to inches
         if(math.fabs(YtdF-YtdR) < 0.25*Rt):
             servos.setSpeedsVW(Kp*(Rt-YtdF),Kp*(Rt-YtdR)*math.pi/6)
-            time.sleep(0.01)
-        else:
+            time.sleep(0.1)
+        elif(math.fabs(YtdF-YtdR) > 0.5*Rt):
             servos.setSpeedsVW(-Kp*(Rt-YtdF),-Kp*(Rt-YtdR)*math.pi/6)
-            time.sleep(0.01)
+            time.sleep(0.1)
 
         #if (YtdF <= 2*Rt):
 	    #getting cornered, WALL NOT SAFE
