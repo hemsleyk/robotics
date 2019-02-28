338b5c3ad89e56fbdfe84c33641fae316eafc0b9
Kyle Hemsley <kyle@hemsley.tech>
2019-02-27 22:10:18 -0500

    data changes

--- wallDistance.py
+++ wallDistance.py
@@ -1,13 +1,12 @@
-from lib import encoders, servos, distance
+from lib import encoders, servos, distance, time
 Rt = 5.00 #desired distance in inches
 
 Kp = float(input("Value of Kp for prop. control:"))
 if (servos.FloatEq(Kp,0)): exit()
 else:
-    Dt = distance.time.monotonic() #access time via distance
+    Dt = time.monotonic() #access time via distance
     while True: #prop control here
         Yt = distance.fSensor.get_distance()/25.4 #convert to inches
         servos.setSpeedsIPS((-Kp*(Rt-Yt)),-Kp*(Rt-Yt))
-        Dt = distance.time.monotonic() - Dt
-        print("Distance: {}\tTime: {}".format(Yt, Dt)) #data read, pipe to file?
-        distance.time.sleep(0.01)
+        print("Distance: {}\tTime: {}".format(Yt, time.monotonic() - Dt)) #data read, pipe to file?
+        time.sleep(0.01)
