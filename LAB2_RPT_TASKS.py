da96f480cd586135686288534fc043ba9af20194
Kyle Hemsley <kyle@hemsley.tech>
2019-02-28 03:37:53 -0500

    Create some code.

--- LAB2_RPT_TASKS.py
+++ LAB2_RPT_TASKS.py
@@ -1,6 +1,6 @@
 import time
 from lib import encoders, servos, distance
-for count in range(1, 100):
+for count in range(1, 30):
     # Print measurement
     print("Front: {}".format(distance.fSensor.get_distance())) #data read, pipe to file via CLI?
     time.sleep(1) #distance has access to time
--- wallFollowing.py
+++ wallFollowing.py
@@ -1,1 +1,26 @@
+import time, math
 from lib import encoders, servos, distance
+
+Rt = 5.0
+
+Kp = float(input("Value of Kp for prop. control:"))
+if (servos.FloatEq(Kp,0)): exit()
+else:
+    Dt = time.monotonic() #access time via distance
+    while True: #prop control here
+        #Yt variables
+        YtdF = distance.fSensor.get_distance()/25.4 #convert to inches
+        YtdR = distance.rSensor.get_distance()/25.4 #convert to inches
+        YtdL = distance.lSensor.get_distance()/25.4 #convert to inches
+        
+        if (YtdF <= 2*Rt):
+	    #getting cornered, WALL NOT SAFE
+            servos.setSpeedsVW(1.25, -math.pi/2)
+        elif (YtdR >= 2*Rt):
+		#wall has disappeared completely
+            servos.setSpeedsVW(2.5, math.pi/2)
+        else:
+		#follow the safe wall
+            servos.setSpeedsIPS(Rt-Kp*(Rt-YtdR),Rt-Kp*(YtdR-Rt))
+        print("Distance: {}\tTime: {}".format(YtdF, time.monotonic() - Dt)) #data read, pipe to file?
+        time.sleep(0.025)
