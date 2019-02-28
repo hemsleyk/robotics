cee639dbfd87afff2a8b8dffaeb96b8681afdb45
Kyle Hemsley <kyle@hemsley.tech>
2019-02-28 04:29:18 -0500

    Timing

--- wallFollowing.py
+++ wallFollowing.py
@@ -14,7 +14,7 @@ else:
         YtdL = distance.lSensor.get_distance()/25.4 #convert to inches
         if(math.fabs(YtdF-YtdR) < 0.1*Rt): #cornered
             servos.setSpeedsVW(-Kp*(Rt-YtdF),-Kp*(Rt+YtdR)*math.pi/6)
-            time.sleep(0.05)
+            time.sleep(0.1)
         else:
             servos.setSpeedsVW(-Kp*(Rt-YtdF),-Kp*(Rt-YtdR)*math.pi/6)
             time.sleep(0.025)
