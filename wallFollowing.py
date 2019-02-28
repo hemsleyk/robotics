6bf2c82ed661b2735b7577dcb7b3d4efd12dc037
Kyle Hemsley <kyle@hemsley.tech>
2019-02-28 05:18:05 -0500

    Revert

--- wallFollowing.py
+++ wallFollowing.py
@@ -14,21 +14,24 @@ else:
         YtdF = distance.fSensor.get_distance()/25.4 #convert to inches
         YtdR = distance.rSensor.get_distance()/25.4 #convert to inches
         YtdL = distance.lSensor.get_distance()/25.4 #convert to inches
-        if(math.fabs(YtdF-YtdR) < 0.25*Rt):
-            servos.setSpeedsVW(Kp*(Rt-YtdF),Kp*(Rt-YtdR)*math.pi/6)
-            time.sleep(0.1)
-        elif(math.fabs(YtdF-YtdR) > 0.5*Rt):
-            servos.setSpeedsVW(-Kp*(Rt-YtdF),-Kp*(Rt-YtdR)*math.pi/6)
-            time.sleep(0.1)
-
-        #if (YtdF <= 2*Rt):
-	    #getting cornered, WALL NOT SAFE
-            #servos.setSpeedsVW(1.25, -math.pi/2)
-        #elif (YtdR >= 2*Rt):
-		#wall has disappeared completely
-            #servos.setSpeedsVW(2.5, math.pi/2)
+        
+        
+        
+        #if(math.fabs(YtdF-YtdR) < 0.25*Rt):
+        #    servos.setSpeedsVW(Kp*(Rt-YtdF),Kp*(Rt-YtdR)*math.pi/6)
+        #    time.sleep(0.1)
         #else:
+        #    servos.setSpeedsVW(-Kp*(Rt-YtdF),-Kp*(Rt-YtdR)*math.pi/6)
+        #    time.sleep(0.1)
+
+        if (YtdF <= 2*Rt):
+	    #getting cornered, WALL NOT SAFE
+            servos.setSpeedsVW(1.25, -math.pi/2)
+        elif (YtdR >= 2*Rt):
+		#wall has disappeared completely
+            servos.setSpeedsVW(2.5, math.pi/12)
+        else:
 		#follow the safe wall
-            #servos.setSpeedsIPS(Rt-Kp*(Rt-YtdR),Rt-Kp*(YtdR-Rt))
+            servos.setSpeedsIPS(Rt-Kp*(Rt-YtdR),Rt-Kp*(YtdR-Rt))
             #servos.setSpeedsIPS((-Kp*(Rt-YtdF)),-Kp*(Rt-YtdF-YtdR))
         #print("Distance: {}\tTime: {}".format(YtdF, time.monotonic() - Dt)) #data read, pipe to file?
