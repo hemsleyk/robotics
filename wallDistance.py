6dfb748f5f5c8f3e86435c8d8e01d22875d6c357
Kyle Hemsley <kyle@hemsley.tech>
2019-02-27 22:08:39 -0500

    inverse

--- wallDistance.py
+++ wallDistance.py
@@ -7,7 +7,7 @@ else:
     Dt = distance.time.monotonic() #access time via distance
     while True: #prop control here
         Yt = distance.fSensor.get_distance()/25.4 #convert to inches
-        servos.setSpeedsIPS((Kp*(Rt-Yt)),Kp*(Rt-Yt))
+        servos.setSpeedsIPS((-Kp*(Rt-Yt)),-Kp*(Rt-Yt))
         Dt = distance.time.monotonic() - Dt
         print("Distance: {}\tTime: {}".format(Yt, Dt)) #data read, pipe to file?
         distance.time.sleep(0.01)
