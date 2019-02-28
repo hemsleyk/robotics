18468426f2d7e99340f192a8065952540095bdca
Kyle Hemsley <kyle@hemsley.tech>
2019-02-27 20:30:18 -0500

    lab task skeleton, 30 second measurement interval?

--- LAB2_RPT_TASKS.py
+++ LAB2_RPT_TASKS.py
@@ -1,29 +1,10 @@
-from lib import encoders, servos
-
-def MoveForward(inches, seconds):
-    encoders.resetCounts()
-    counts = encoders.getCounts()
-    ips = inches/seconds
-    if(ips > servos.LINEAR_V_MAX):
-        print("Requested action exceeds theoretical maximum velocity")
-        print("Maximum linear velocity:",servos.LINEAR_V_MAX)
-        print("Requested linear velocity:",ips)
-        return 1
-    else: #stop at ticks*IN_PER_TICK>=inches
-        print("Beginning forwards motion of",inches,"over",seconds,"seconds with a velocity of",ips,"in/s")
-        servos.setSpeedsIPS(ips,ips) #set velocity
-        while(counts[0] <= inches/servos.IN_PER_TICK or counts[1] <= inches/servos.IN_PER_TICK): counts = encoders.getCounts()
-        servos.setSpeeds(0,0)
-        return 0
-
-while True:
-    inches = float(input("Distance to move in inches, 0 to quit:"))
-    if (servos.FloatEq(inches,0)): exit()
-    else:
-        
-        seconds = float(input("Time in seconds to complete maneuver:"))
-        if (servos.FloatEq(seconds,0)):
-            print("Seconds is a divisor and cannot be zero")
-            exit()
-        else:
-            MoveForward(inches,seconds)
+from lib import encoders, servos, distance
+for count in range(1, 30):
+#    # Get a measurement from each sensor
+    lDistance = distance.lSensor.get_distance()
+    fDistance = distance.fSensor.get_distance()
+    rDistance = distance.rSensor.get_distance()
+    
+    # Print each measurement
+    print("Left: {}\tFront: {}\tRight: {}".format(lDistance, fDistance, rDistance)) #data read, pipe to file?
+    distance.time.sleep(1) #distance has access to time
--- lib/distance.py
+++ lib/distance.py
@@ -52,16 +52,6 @@ GPIO.output(FSHDN, GPIO.HIGH)
 time.sleep(0.01)
 fSensor.start_ranging(VL53L0X.VL53L0X_GOOD_ACCURACY_MODE)
 
-
-for count in range(1, 100):
-    # Get a measurement from each sensor
-    lDistance = lSensor.get_distance()
-    fDistance = fSensor.get_distance()
-    rDistance = rSensor.get_distance()
-    
-    # Print each measurement
-    print("Left: {}\tFront: {}\tRight: {}".format(lDistance, fDistance, rDistance))
-
 # Stop measurement for all sensors
 lSensor.stop_ranging()
 fSensor.stop_ranging()
