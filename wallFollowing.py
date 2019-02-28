68ae13dbbf87b86d1301984ca7f65e953f1eca51
Kyle Hemsley <kyle@hemsley.tech>
2019-02-28 04:39:22 -0500

    Loosen

--- wallFollowing.py
+++ wallFollowing.py
@@ -14,10 +14,10 @@ else:
         YtdL = distance.lSensor.get_distance()/25.4 #convert to inches
         if(math.fabs(YtdF-YtdR) < 0.25*Rt):
             servos.setSpeedsVW(Kp*(Rt-YtdF),Kp*(Rt-YtdR)*math.pi/6)
-            time.sleep(0.025)
-        else:
+            time.sleep(0.1)
+        else
             servos.setSpeedsVW(-Kp*(Rt-YtdF),-Kp*(Rt-YtdR)*math.pi/6)
-            time.sleep(0.025)
+            time.sleep(0.1)
 
         #if (YtdF <= 2*Rt):
 	    #getting cornered, WALL NOT SAFE
