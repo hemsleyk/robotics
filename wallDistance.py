54d4d4a80ce9a408c8695d8c99651baccedc2a3f
Kyle Hemsley <kyle@hemsley.tech>
2019-02-27 22:06:26 -0500

    unit fixes

--- wallDistance.py
+++ wallDistance.py
@@ -6,7 +6,7 @@ if (servos.FloatEq(Kp,0)): exit()
 else:
     Dt = distance.time.monotonic() #access time via distance
     while True: #prop control here
-        Yt = distance.fSensor.get_distance()
+        Yt = distance.fSensor.get_distance()/25.4 #convert to inches
         servos.setSpeedsIPS((Kp*(Rt-Yt)),Kp*(Rt-Yt))
         Dt = distance.time.monotonic() - Dt
         print("Distance: {}\tTime: {}".format(Yt, Dt)) #data read, pipe to file?
