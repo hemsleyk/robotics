a3cd6d2d5d6a350fafc76ac5f097e1fbb45bc978
Kyle Hemsley <kyle@hemsley.tech>
2019-03-27 22:36:28 -0400

    Test sort

--- faceGoal.py
+++ faceGoal.py
@@ -1,12 +1,13 @@
 from lib import servos, camera, blob
-
+goalX = 0.0
+GoalFound = False
 def FrameGoal():
     print("Goal is not visible, seeking.")
-    servos.setSpeeds(50,-50) #set velocity
+    #servos.setSpeeds(50,-50) #set velocity
     return 0
 
 while True:
-    GoalFound = False
+    detectedBlobs = sorted(blob.keypoints, key=lambda keypoint: keypoint.response,reverse=True)
     while GoalFound: #center the goal, stay stationary, use propcon
         print("wee")
     FrameGoal() #goal isn't visible
