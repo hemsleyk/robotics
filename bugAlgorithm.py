9b722e714908680e1a9595c08b4a4b088374115f
Kyle Hemsley <kyle@hemsley.tech>
2019-04-05 02:43:21 -0400

    radical idea

--- bugAlgorithm.py
+++ bugAlgorithm.py
@@ -40,13 +40,16 @@ def SeekGoal(): #either rotate to goal or move to goal
         else:
             while(ExecCV()): #as long as we can see goal
                 if math.fabs(RtWs-YtdF) > deadzoneWs and (YtCsY < 100 or YtCsD <= 200): #need to approach
-                    servos.setSpeedsIPS(-Kp*(RtWs-YtdF),-Kp*(RtWs-YtdF)) #correct distance to the goal
-                    print("approaching goal")
-                    print("Distance (in): ", RtWs, "actual (in): ", YtdF)
-                    print("height (px): ", YtCsY)
-                    print("diameter (px): ", YtCsD)
-                elif YtdF*2.54 < 10 and YtCsD < 200: #non-goal wall must be in front.
-                    WallFollowing()
+                    if YtdF*2.54 < 10 and YtCsD < 200: #non-goal wall must be in front.
+                        WallFollowing()
+                    else
+                        servos.setSpeedsIPS(-Kp*(RtWs-YtdF),-Kp*(RtWs-YtdF)) #correct distance to the goal
+                        print("approaching goal")
+                        print("Distance (in): ", RtWs, "actual (in): ", YtdF)
+                        print("height (px): ", YtCsY)
+                        print("diameter (px): ", YtCsD)
+                elif YtdF*2.54 < 10 and YtCsD < 200:
+                    WallFollowing() #safety catch
                 else: #must be at goal
                     print("at the goal")
                     print("Distance (in): ", RtWs, "actual (in): ", YtdF)
